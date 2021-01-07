f59b02cbf3fe01b2bf9163db830d3afa543ef2fa
blupine <skwint11@gmail.com>
2021-01-07 22:42:54 +0900

    Initial commit

---
+++ src/main/java/jpabook/jpashop/SpringJpaWebshopExampleApplication.java
@@ -0,0 +1,13 @@
+package jpabook.jpashop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJpaWebshopExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJpaWebshopExampleApplication.class, args);
+	}
+
+}
