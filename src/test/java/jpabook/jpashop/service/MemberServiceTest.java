c86181b142d9f7c4c018e22d7473df756c7e466a
blupine <skwint11@gmail.com>
2021-01-11 22:06:31 +0900

    Add MemberService test cases

---
+++ src/test/java/jpabook/jpashop/service/MemberServiceTest.java
@@ -0,0 +1,48 @@
+package jpabook.jpashop.service;
+
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.repository.MemberRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@Transactional
+class MemberServiceTest {
+
+    @Autowired MemberRepository memberRepository;
+    @Autowired MemberService memberService;
+
+    @Test
+    public void 회원가입() throws Exception {
+        // given
+        Member member = new Member();
+        member.setName("testName");
+
+        // when
+        Long id = memberService.join(member);
+
+        // then
+        assertEquals(member, memberRepository.findOne(id));
+    }
+
+    @Test
+    public void 중복_회원가입() throws Exception {
+        // given
+        Member member = new Member();
+        member.setName("testName");
+
+        Member member2 = new Member();
+        member2.setName("testName");
+
+        // when
+        memberService.join(member);
+        IllegalStateException thrown = assertThrows(IllegalStateException.class, () -> memberService.join(member2));
+
+        // then
+        assertEquals(thrown.getMessage(), "이미 존재하는 회원입니다.");
+    }
+}
