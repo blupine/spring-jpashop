72fbf69288ff10902f06e15a3ee51c4763e725e3
blupine <skwint11@gmail.com>
2021-01-13 19:37:12 +0900

    Add basic method for Item repository

---
+++ src/main/java/jpabook/jpashop/repository/ItemRepository.java
@@ -0,0 +1,39 @@
+package jpabook.jpashop.repository;
+
+import jpabook.jpashop.domain.item.Item;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import java.util.List;
+
+@Repository
+@RequiredArgsConstructor
+public class ItemRepository {
+
+    private final EntityManager em;
+
+    public void save(Item item) {
+        if (item.getId() == null) { // 비영속 상태일 때
+            em.persist(item);
+        } else {    // 준영속 상태일 때 -> 영속으로 만들어줌
+            em.merge(item);
+        }
+    }
+
+
+    public Item findOne(Long id) {
+        return em.find(Item.class, id);
+    }
+
+    public List<Item> findAll() {
+        return em.createQuery("select i from Item i", Item.class)
+                .getResultList();
+    }
+
+    public List<Item> findByName(String name) {
+        return em.createQuery("select i from Item i where i.name = :name", Item.class)
+                .setParameter("name", name)
+                .getResultList();
+    }
+}
