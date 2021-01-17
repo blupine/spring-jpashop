bf1de6a1349fe1611c9de610fe91810438963038
blupine <skwint11@gmail.com>
2021-01-17 16:16:16 +0900

    Fix unloading filed issue through force loading

--- src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
+++ src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
@@ -29,6 +29,10 @@ public class OrderSimpleApiController {
     @GetMapping("/api/v1/simple-orders")
     public List<Order> ordersV1() {
         List<Order> all = orderRepository.findAllByString(new OrderSearch());
+        for (Order order : all) {
+            order.getMember().getName();        // Lazy Loading 필드 강제 로딩
+            order.getDelivery().getAddress();   // Lazy Loading 필드 강제 로딩
+        }
         return all;
     }
 }
