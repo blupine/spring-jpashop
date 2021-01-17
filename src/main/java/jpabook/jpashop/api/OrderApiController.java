68910522e0f59e978714b0d1894ca1be92c5d152
blupine <skwint11@gmail.com>
2021-01-17 20:12:34 +0900

    Use fetch join for orders request

--- src/main/java/jpabook/jpashop/api/OrderApiController.java
+++ src/main/java/jpabook/jpashop/api/OrderApiController.java
@@ -42,6 +42,16 @@ public class OrderApiController {
         return new Result(collect);
     }
 
+    @GetMapping("/api/v3/orders")
+    public Result ordersV3() {
+        List<Order> orders = orderRepository.findAllWithItem();
+        List<OrderDto> collect = orders.stream()
+                .map(o -> new OrderDto(o))
+                .collect(Collectors.toList());
+        return new Result(collect);
+    }
+
+
     @Data
     static class OrderDto {
         private Long orderId;
