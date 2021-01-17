dd14c2fef37613b7e5065246d31895240eae9b9c
blupine <skwint11@gmail.com>
2021-01-17 16:33:32 +0900

    Remove hibernate5Module

--- src/main/java/jpabook/jpashop/SpringJpaWebshopExampleApplication.java
+++ src/main/java/jpabook/jpashop/SpringJpaWebshopExampleApplication.java
@@ -1,9 +1,7 @@
 package jpabook.jpashop;
 
-import com.fasterxml.jackson.datatype.hibernate5.Hibernate5Module;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SpringJpaWebshopExampleApplication {
@@ -11,10 +9,4 @@ public class SpringJpaWebshopExampleApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringJpaWebshopExampleApplication.class, args);
 	}
-
-	@Bean
-	Hibernate5Module hibernate5Module() {
-		return new Hibernate5Module();
-	}
-
 }
